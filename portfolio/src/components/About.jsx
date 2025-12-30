14b6dbccef88b6bf220840f64b819865141faa4a
Harmeet Singh <harmees2@uci.edu>
2025-12-30 11:31:47 -0800

    Add About component to display personal information

---
+++ portfolio/src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <section className="about">
+            <h4>About me</h4>
+            <p>My name is Harmeet Singh</p>
+        </section>
+    )
+}
